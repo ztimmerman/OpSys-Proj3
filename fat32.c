b7db3174dbaf6a80ac5fd5a1be8e7de074624ded
Zachary Timmerman <zwt13@my.fsu.edu>
2018-04-29 21:57:58 -0400

    Updated DIR_ATTR of creat to give value of 0x00 for making files.

--- fat32.c
+++ fat32.c
@@ -1248,7 +1248,7 @@ void creat()    {
                                         if(name[i/2]=='\0')
                                                 break;
                                 }
-        //              dir.DIR_Attr=0x10;
+																dir.DIR_Attr = 0x00;
                                 dir.DIR_FileSize = 0;
                                 dir.DIR_FstClusHI=emptyClus/0x100;
                                 dir.DIR_FstClusLO=emptyClus%0x100;
@@ -1293,7 +1293,72 @@ void creat()    {
 
 /********************************RM********************************/
 void rm()	{
-	
+	unsigned int offset;
+	unsigned int c = currClus;
+	bool found = 0;
+
+	//get new file's name
+	scanf("%s",name);
+
+	// make sure the user isn't typing something forbidden
+	if (strcmp(name,"..") == 0)     {
+		printf("Invalid argument\n");
+		return;
+	} else if (strcmp(name,".") == 0) {
+		printf("Invalid argument\n");
+		return;
+	}
+
+	//seek to current cluster
+	offset = SectorOffset(FirstSectorCluster(c));
+	fseek(file,offset,SEEK_SET);
+	unsigned int temp = offset + bpb.BPB_BytsPerSec * bpb.BPB_SecPerClus;
+
+	while (temp > offset)   {
+		//fill dir structs
+		fread(&ldir,sizeof(struct LongDirectoryEntry),1,file);
+		fread(&dir,sizeof(struct DirectoryEntry),1,file);
+
+		unsigned char fname[26]={0};
+		for (int i = 0, k = 0; i < 10; i += 2)  {
+			//concatenates filenames from read data //!!! May need refactoring !!!
+			fname[k]=ldir.LDIR_Name1[i];
+			if(fname[k]=='\0')
+				break;
+				k++;
+				if(i==8){
+				for(int j=0;j<12;j+=2){
+					fname[k]=ldir.LDIR_Name2[j];
+					if(fname[k]=='\0')
+						break;
+					k++;
+					if(j==10){
+						for(int l=0;l<4;l+=2){
+							fname[k]=ldir.LDIR_Name3[l];
+							if(fname[k]=='\0')
+								break;
+							k++;
+						}
+					}
+				}
+			}
+		}
+		//trigger on name match
+		if(strcmp(fname,name)==0){
+			found=1;
+			break;
+		}
+		offset+=64;     //increments to next entry
+	}//end while
+
+	//if user is trying to rm file that doesn't exist
+	if (!found)	{
+		printf("File doesn't exist.\n");
+		return;
+	}
+
+	//if file exists...
+	printf("rm not fully implemented. Cannot delete files.\n");
 }
 
 
@@ -1349,6 +1414,10 @@ int main(int argc, char*argv[]){
           creat();
           while((getchar())!='\n');
         }
+				else if (strcmp(cmd,"rm")==0)	{
+					rm();
+					while((getchar()) != '\n');
+				}
 	else if(strcmp(cmd,"open")==0){
 	  open();
 	  while((getchar())!='\n');
